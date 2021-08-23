de6d69c3a8cf3d9d9742221f7a38daeadbbca176
Oliver Fink <323250+olifink@users.noreply.github.com>
2021-08-23 18:02:33 +0200

    cleanup

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-import { msg } from "./something.js";
 import { Button } from '@syncfusion/ej2-buttons';
 
 // initialize button control
@@ -7,7 +6,3 @@ let button: Button = new Button();
 // render initialized button
 button.appendTo('#normalbtn');
 
-//-------
-const el = document.createElement('h1');
-el.textContent = msg;
-document.body.appendChild(el);
